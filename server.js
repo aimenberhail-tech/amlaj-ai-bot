9c3fc9c04900fec5e3ea2b4cbb16f5e80fce2020
aimenberhail-tech <aimenberhail@gmail.com>
2026-06-16 17:06:22 +0100

    Update server.js

--- server.js
+++ server.js
@@ -11,14 +11,12 @@ app.get("/webhook", (req, res) => {
   const token = req.query["hub.verify_token"];
   const challenge = req.query["hub.challenge"];
 
-  if (mode && token) {
-    if (mode === "subscribe" && token === VERIFY_TOKEN) {
-      console.log("Webhook verified");
-      res.status(200).send(challenge);
-    } else {
-      res.sendStatus(403);
-    }
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("Webhook verified");
+    return res.status(200).send(challenge);
   }
+
+  return res.status(200).send("Webhook endpoint is ready");
 });
 
 app.post("/webhook", (req, res) => {
